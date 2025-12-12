2302287b70f45d39d946a1cc77597e9d5b9bdc1b
ReanGD <reangd@yandex.ru>
2025-12-12 03:35:00 +0300

    add make_response_complete

--- libs/kit/src/logiclab_kit/schemas/responses_api/__init__.py
+++ libs/kit/src/logiclab_kit/schemas/responses_api/__init__.py
@@ -1,8 +1,10 @@
 from .create import CreateRequest, AgnoCreateRequest
 from .response_stream import ResponseStream
+from .response_complete import make_response_complete
 
 __all__ = [
     "CreateRequest",
     "ResponseStream",
     "AgnoCreateRequest",
+    "make_response_complete",
 ]
